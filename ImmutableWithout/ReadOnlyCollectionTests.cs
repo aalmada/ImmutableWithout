3b57a3d479ecdf8b4ae542ee16e922777ad8e673
Antao Almada <antao.almada@sky.uk>
2017-03-01 13:24:24 +0000

    Added ReadOnlyCollectionTests. Changed from NUnit to VSTest.

---
+++ ImmutableWithout/ReadOnlyCollectionTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ploeh.AutoFixture;
+using System.Collections.Generic;
+using Ploeh.AutoFixture.Kernel;
+
+namespace ImmutableWithout
+{
+    [TestClass]
+    public class ReadOnlyCollectionTests
+    {
+        class Asset
+        {
+            public AssetMetadata Metadata { get; }
+
+            public Asset(AssetMetadata metadata)
+            {
+                Metadata = metadata;
+            }
+        }
+
+        class AssetMetadata
+        {
+            public IReadOnlyCollection<Asset> Assets { get; }
+
+            public AssetMetadata(IReadOnlyCollection<Asset> assets)
+            {
+                Assets = assets;
+            }
+        }
+
+        [TestMethod]
+        public void FixtureCreateAssetMetadata_Should_Succeed()
+        {
+            var fixture = new Fixture();
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+            fixture.Customizations.Add(
+                new TypeRelay(
+                    typeof(IReadOnlyCollection<Asset>),
+                    typeof(List<Asset>)));
+
+            var metadata = fixture.Create<AssetMetadata>();
+            Assert.IsNotNull(metadata);
+            Assert.AreNotEqual(0, metadata.Assets.Count);
+        }
+    }
+}
--- ImmutableWithout/WithoutTests.cs
+++ ImmutableWithout/WithoutTests.cs
@@ -1,15 +1,11 @@
-﻿using NUnit.Framework;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Ploeh.AutoFixture;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ImmutableWithout
 {
-    [TestFixture]
-    public class Tests
+    [TestClass]
+    public class WithoutTests
     {
         class MyImmutableClass
         {
@@ -21,35 +17,35 @@ namespace ImmutableWithout
             }
         }
 
-        [Test]
+        [TestMethod]
         public void FixtureCreate_Should_Succeed()
         {
             var fixture = new Fixture();
             var instance = fixture.Create<MyImmutableClass>();
-            Assert.That(instance, Is.Not.Null);
-            Assert.That(instance.MyProperty, Is.Not.EqualTo(0));
+            Assert.IsNotNull(instance);
+            Assert.AreNotEqual(0, instance.MyProperty);
         }
 
-        [Test]
+        [TestMethod]
         public void FixtureCreate_Should_Succeed_When_WithouInCreate()
         {
             var fixture = new Fixture();
             var instance = fixture.Build<MyImmutableClass>()
                 .Without(i => i.MyProperty)
                 .Create();
-            Assert.That(instance, Is.Not.Null);
-            Assert.That(instance.MyProperty, Is.EqualTo(0));
+            Assert.IsNotNull(instance);
+            Assert.AreEqual(0, instance.MyProperty);
         }
 
-        [Test]
+        [TestMethod]
         public void FixtureCreate_Should_Succeed_When_WithouInCustomize()
         {
             var fixture = new Fixture();
             fixture.Customize<MyImmutableClass>(composer => composer
                 .Without(i => i.MyProperty));
             var instance = fixture.Create<MyImmutableClass>();
-            Assert.That(instance, Is.Not.Null);
-            Assert.That(instance.MyProperty, Is.EqualTo(0));
+            Assert.IsNotNull(instance);
+            Assert.AreEqual(0, instance.MyProperty);
         }
     }
 }
